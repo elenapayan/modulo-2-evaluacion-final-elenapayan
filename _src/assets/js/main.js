1c8007b6e77f2ea8ba787c6593232e56bcc8e65e
elenapayan <elena.payan.glez@gmail.com>
2019-12-02 12:05:51 +0100

    Add toggle to the background-color of list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -13,24 +13,25 @@ function getMovies() {
         })
         .then(function (data) {
             paintMovies(data);
+            activateToggle();
         })
 }
 
 function paintMovies(data) {
-    let list = "";
+    let list = [];
     for (let i = 0; i < data.length; i++) {
         if (data[i].show.image === null) {
             data[i].show.image = {
                 medium: "http://via.placeholder.com/210x295"
             }
         };
-        list += '<li class="container">'
+        list += '<li class="container js-content">'
         list += `<img class="js-image" src="${data[i].show.image.medium}" alt="Foto ${data[i].show.name}"/>`
         list += `<h4 class="title">${data[i].show.name}</h4>`
         list += '</li>'
     }
     const ulList = document.querySelector(".js-right-list");
-    ulList.innerHTML = list;
+    ulList.innerHTML += list;
 }
 
 function getResults(event) {
@@ -38,3 +39,15 @@ function getResults(event) {
     getMovies();
 }
 btn.addEventListener("click", getResults);
+
+
+function activateToggle() {
+    const content = document.querySelectorAll(".js-content");
+    for (let i = 0; i < content.length; i++) {
+        content[i].addEventListener("click", toggleClass);
+    }
+}
+
+function toggleClass(event) {
+    event.currentTarget.classList.toggle("change-color");
+}
