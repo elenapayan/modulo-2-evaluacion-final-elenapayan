da9584f78157f88862724c4146fea1601dc09cff
elenapayan <elena.payan.glez@gmail.com>
2019-12-02 09:33:33 +0100

    Create basic HTML and JS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-console.log('>> Ready :)');
+const btn = document.querySelector(".js-btn");
+const input = document.querySelector(".js-input");
+const ulElement = document.querySelector(".js-list");
+let list = [];
+
+function getMovies() {
+    fetch(`http://api.tvmaze.com/search/shows?q=${input.value}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            paintMovies(data);
+        })
+}
+
+function paintMovies(data) {
+    for (let i = 0; i < data.length; i++) {
+        list += '<li>'
+        list += `<img src="${data[i].show.image.medium}" alt="Foto ${data[i].show.name}"/>`
+        list += `<h4>${data[i].show.name}</h4>`
+        list += '</li>'
+    }
+
+    ulElement.innerHTML = list;
+}
+
+function getResults(event) {
+    event.preventDefault();
+    getMovies();
+}
+btn.addEventListener("click", getResults);
