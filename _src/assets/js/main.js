0a2dc19a71c6e55331d18412bc885101dffb79eb
elenapayan <elena.payan.glez@gmail.com>
2019-12-03 14:27:27 +0100

    Add localStorage function and favorite color

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,22 +4,23 @@ const btn = document.querySelector(".js-btn");
 const input = document.querySelector(".js-input");
 const ulList = document.querySelector(".js-right-list");
 const ulFavorites = document.querySelector(".js-left-list");
-let favoriteIdList = [];
 let favoriteList = [];
+let data = [];
 let list = "";
-
+let favoriteObject;
 
 function getMovies() {
     fetch(`http://api.tvmaze.com/search/shows?q=${input.value}`)
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            paintMovies(data);
+        .then(function (serverData) {
+            data = serverData;
+            paintMovies();
             activateToggle();
         })
 }
-function paintMovies(data) {
+function paintMovies() {
     list = "";
     ulList.innerHTML = "";
     for (let i = 0; i < data.length; i++) {
@@ -28,7 +29,14 @@ function paintMovies(data) {
                 medium: "http://via.placeholder.com/210x295"
             }
         };
-        list += `<li class="right-list-container js-content" id="${data[i].show.id}">`
+
+        let favClass = '';
+        const favIndex = getFavIndex(data[i].show.id);
+        if (favIndex !== -1) {
+            favClass = "change-color";
+        };
+
+        list += `<li class="js-content right-list-container ${favClass}" id="${data[i].show.id}">`
         list += `<img class="js-image image" src="${data[i].show.image.medium}" alt="Foto ${data[i].show.name}"/>`
         list += `<h4 class="title">${data[i].show.name}</h4>`
         list += '</li>'
@@ -45,20 +53,63 @@ btn.addEventListener("click", getResults);
 function activateToggle() {
     const content = document.querySelectorAll(".js-content");
     for (let i = 0; i < content.length; i++) {
-        content[i].addEventListener("click", toggleClass);
+        content[i].addEventListener("click", addFavorites);
     }
 }
 
-function toggleClass(event) {
-    event.currentTarget.classList.toggle("change-color");
-    paintFavorites(event);
+function paintFavs() {
+    list = "";
+    ulFavorites.innerHTML = "";
+    for (let i = 0; i < favoriteList.length; i++) {
+        list += `<li class="js-content left-list-container" id="${favoriteList[i].id}">`
+        list += `<img class="js-image image" src="${favoriteList[i].image}" alt="Foto ${favoriteList[i].name}"/>`
+        list += `<h4 class="title">${favoriteList[i].name}</h4>`
+        list += '</li>'
+    }
+    ulFavorites.innerHTML += list;
 }
 
-function paintFavorites(event) {
-    let favoriteMovie = event.currentTarget.innerHTML;
-    if (!favoriteIdList.includes(event.currentTarget.id)) {
-        ulFavorites.innerHTML += `<li class="left-list-container" data-id="${event.currentTarget.id}">${favoriteMovie}</li>`;
-        favoriteIdList.push(event.currentTarget.id);
+function addFavorites(event) {
+
+    let favoriteName = event.currentTarget.querySelector(".title");
+    let favoriteImage = event.currentTarget.querySelector(".js-image");
+
+    favoriteObject = {
+        id: event.currentTarget.id,
+        name: favoriteName.textContent,
+        image: favoriteImage.src,
+    };
+
+    let favIndex = getFavIndex(favoriteObject.id);
+
+    if (favIndex === -1) {
+        favoriteList.push(favoriteObject);
+    } else {
+        favoriteList.splice(favIndex, 1);
+    }
+
+    localStorage.setItem("favoriteList", JSON.stringify(favoriteList));
+    paintFavs();
+    paintMovies();
+    activateToggle();
+};
+
+function getFavIndex(id) {
+    for (let i = 0; i < favoriteList.length; i++) {
+        if (parseInt(favoriteList[i].id) === parseInt(id)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function getLocalStorage() {
+    const localStorageFavoriteListJSON = localStorage.getItem("favoriteList");
+    const localStorageFavoriteList = JSON.parse(localStorageFavoriteListJSON);
+    if (localStorageFavoriteList !== null) {
+        favoriteList = localStorageFavoriteList;
+        paintFavs();
     }
 }
 
+getLocalStorage();
